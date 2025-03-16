38727e9aa1c1dee3845c3f85c2f22051c8625773
Ajaykumar-2246 <131764062+Ajaykumar-2246@users.noreply.github.com>
2025-03-16 18:33:58 +0530

    my native app backend

--- src/config/cron.js
+++ src/config/cron.js
@@ -2,7 +2,7 @@ import cron from "cron";
 import http from "http";
 
 const job = new cron.CronJob("*/14 * * * *", async () => {
-  http.get(process.env.API_REFRESH_URL, (res) => {
+  https.get(process.env.API_REFRESH_URL, (res) => {
     if (res.statusCode === 200) {
       console.log("API refreshed");
     } else {
