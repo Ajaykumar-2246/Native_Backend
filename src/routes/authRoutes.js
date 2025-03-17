4a37e4b2cae5a246ac5cf46506da280c48e86ade
Ajaykumar-2246 <131764062+Ajaykumar-2246@users.noreply.github.com>
2025-03-17 12:33:39 +0530

    my native app backend

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
+import { protectRoutes } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -89,6 +90,18 @@ router.post("/login", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/loggedInUser", protectRoutes, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
   }
 });
 
